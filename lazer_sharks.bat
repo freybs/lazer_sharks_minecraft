fc492355ac92f4a5e87d68bd74bf69b07d3cef97
freybs <nickfreybler@gmail.com>
2019-08-28 23:06:07 -0700

    try again...

--- lazer_sharks.bat
+++ lazer_sharks.bat
@@ -1,3 +1,3 @@
 H:
-cd minecraft\lazer_sharks_server
+cd minecraft\lazer_sharks_server1
 java -d64 -server -Xms1G -Xmx8G -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -jar .\server.jar nogui
