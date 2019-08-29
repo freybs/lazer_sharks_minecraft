e69a0e212b8f8448063235ba82bbc9696b1fc23a
GundamMerlin <gundammerlin@gmail.com>
2019-08-29 03:18:54 -0500

    test push

--- francis_startup.bat
+++ francis_startup.bat
@@ -1,3 +1,3 @@
 D:
 cd Lazer Shark Server
-java -d64 -server -Xms1G -Xmx8G -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -jar .\server.jar nogui
+java -d64 -server -Xms1G -Xmx4G -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -jar .\server.jar nogui
